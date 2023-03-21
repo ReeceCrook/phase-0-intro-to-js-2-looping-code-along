cd3d35574fdfc657cda9c02ecdf1bf2a77d01607
Reece Crook <reececrook16@gmail.com>
2023-03-21 14:37:51 -0600

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(names, surp){
+    let message = []
+    for (let i = 0; i < names.length; i++){
+        message.push(`Thank you, ${names[i]}, for the wonderful ${surp} gift!`);
+        //console.log(message)
+        //console.log(message)
+    }
+    return message
+        //console.log(message);
+        //console.log(message.length)
+        //console.log(names)
+        //console.log(names.length)
+        
+    //console.log(mes)
+    //console.log(mes.length)
+    //console.log(names)
+    //console.log(names.length);
+}
+
+writeCards(names, "surprise")
+
+function countDown(number){
+    while(-1 < number){
+        console.log(number--)
+    }
+
+}
+countDown(10)
